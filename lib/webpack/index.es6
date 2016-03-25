0f9b0617d27104649d89a819de0192d865365bcb
Allison Zadrozny <allison@zadrozny.com>
2016-03-25 14:04:00 -0500

    Add babel and chg input files to es6

---
+++ lib/webpack/index.es6
@@ -0,0 +1,1 @@
+require('./hello.es6');
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,16 @@
 module.exports = {
-  entry: "./lib/webpack/index.js",
+  entry: "./lib/webpack/index.es6",
   output: {
     path: __dirname + "/lib/assets/javascripts",
     filename: "bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.es6?$/,
+        exclude: /node_modules/,
+        loader: 'babel' // 'babel-loader' is also a legal name to reference
+      }
+    ]
   }
 };
